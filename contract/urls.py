2f8896ee157bfede9daf1a17c9de6a397bb15258
ruslan-kornich <korny4rus143@gmail.com>
2023-02-26 14:17:25 +0200

    urls

--- contract/urls.py
+++ contract/urls.py
@@ -4,4 +4,6 @@ from contract import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("create/", views.create, name="create"),
+    path("no-data/", views.no_data, name="no-data"),
 ]
